8692f9ce9a08105f91063123c3338d781ebc06f0
Josh Boykin <josh@boyk.in>
2022-06-28 23:29:54 -0400

    Added Logo as well as Menu and Inventory files

---
+++ MenuLoop.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using Colorful;
+//using Console = Colorful.Console;
+
+namespace AIMS
+{
+    class MenuLoop
+    {
+        //public static void Menu()
+        //{
+        //    for (; ; )
+        //    {
+        //        Console.Clear();
+        //        WriteLogo();
+        //        Console.WriteLine("Please choose one of the following options.");\
+        //        Console.WriteLine();
+        //    }
+        //}
+        //public static void Say(string prefix, string message)
+        //{
+        //    Console.Write("[");
+        //    Console.Write(prefix, Color.White);
+        //    Console.Write("] " + message);
+        //    Console.WriteLine();
+        //}
+
+        public static void WriteLogo()
+        {
+            string logo = @"
+                                                                      ________
+                  *         ******  **********  ******               /  ____  \
+                 ***          **    **  **  **  **                  /  / __ \  \          
+                ** **         **    **  **  **  **                 /  / /  \ \  \    
+               **   **        **    **  **  **  ******            /  / / /\_\_\__\_____////
+              *********       **    **      **  ******            \  \ \ \/ / /  /     \\\\       
+             **       **      **    **      **      **             \  \ \__/ /  /      
+            **         **     **    **      **      **              \  \____/  /
+           **           **  ******  **      **  ******               \________/
+";
+            Console.WriteLine(logo, Color.Red);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,24 @@
-﻿namespace AIMS
+﻿using System;
+
+namespace AIMS
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+            Console.Clear();
+            MenuLoop.WriteLogo();
+            Thread.Sleep(2000);
+            Console.WriteLine("Welcome to the Alcohol Inventory Managment System.");
+            Thread.Sleep(2000);
+            Console.WriteLine();
+            Console.WriteLine("What would you like to do?");
+        }    
     }
 }
+
+
+
+
+
+
