d12b9a32e19c4e00bf8efb2cecccf11760d8175d
Josh Boykin <josh@boyk.in>
2022-07-29 11:25:30 -0400

    Bugfix - typo

--- ProductMap.cs
+++ ProductMap.cs
@@ -9,7 +9,7 @@ namespace AIMS
         {
             Map(m => m.Name).Name("Name");
             Map(m => m.Price).Name("Price");
-            Map(m => m.Price).Name("Price");
+            Map(m => m.Quantity).Name("Quantity");
             Map(m => m.QuantityPrice).Name("QuantityPrice");
         }
     }
